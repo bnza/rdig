3dad5c1a3e52f619f95e272367a9acce96e4ce30
Pietro Baldassarri <pietro.baldassarri@gmail.com>
2018-03-06 19:09:44 +0000

    Cleaned project

--- assets/js/old/src/components/DataFormSiteCreate.vue
+++
@@ -1,8 +0,0 @@
-<script>
-  import DataFormSiteEdit from './DataFormSiteEdit'
-
-  export default {
-    name: "data-form-site-create",
-    extends: DataFormSiteEdit
-  }
-</script>
--- assets/js/old/src/mixins/AuthorizationHelperMixin.js
+++
@@ -1,8 +0,0 @@
-export default {
-  methods: {
-    authorize: function (path) {
-      return true
-      //return this.$store.getters['account/isAuthorized'](path, this.$router)
-    }
-  }
-}
